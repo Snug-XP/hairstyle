c4e3618e18824b886077acd2ba526519102a6a88
1055386758 <1055386758@qq.com>
2019-10-26 17:10:53 +0800

    完成分页获取待审核的发型文章方法； 完成同意或拒绝发型文章的发表方法；

--- src/main/java/com/gaocimi/flashpig/service/ArticleService.java
+++ src/main/java/com/gaocimi/flashpig/service/ArticleService.java
@@ -2,6 +2,7 @@ package com.gaocimi.flashpig.service;
 
 import java.util.List;
 import com.gaocimi.flashpig.entity.Article;
+import com.gaocimi.flashpig.entity.Article;
 import org.springframework.data.domain.Page;
 
 /**
@@ -22,4 +23,13 @@ public interface ArticleService {
     public void delete(int id);
 
     public Page<Article> findAll(int pageNum,int pageSize);
+
+    /**
+     * 分页获取待审核的发型文章
+     *
+     * @param pageNum  页数（第几页）
+     * @param pageSize 每页大小
+     * @return
+     */
+    public Page<Article> findPendingList(int pageNum, int pageSize);
 }
