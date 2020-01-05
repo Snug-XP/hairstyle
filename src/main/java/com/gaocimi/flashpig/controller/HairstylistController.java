fafb641780242f3166edcc35a85363db81f07409
1055386758 <1055386758@qq.com>
2020-01-05 23:01:59 +0800

    修改发型师推荐（单个或者列表）接口的范围设置，添加距离的接口，让前端可调推荐的范围

--- src/main/java/com/gaocimi/flashpig/controller/HairstylistController.java
+++ src/main/java/com/gaocimi/flashpig/controller/HairstylistController.java
@@ -1268,14 +1268,15 @@ public class HairstylistController {
         }
     }
 
-    @ApiOperation(value = "根据经纬度获取推荐的发型师", notes = "从附近门店（经纬度相差0.05即约附近5公里左右）中随机选取一位发型师")
+    @ApiOperation(value = "根据经纬度获取推荐的一位发型师", notes = "从附近门店中随机选取一位发型师,distance表示获取附近的范围距离（单位：公里）")
     @GetMapping("/user/getHairstylistRecommendedList")
     public Map getHairstylistRecommendedList(@RequestParam Double longitude,
-                                             @RequestParam Double latitude) {
+                                             @RequestParam Double latitude,
+                                             @RequestParam(value = "distance",defaultValue = "5") Integer distance) {
         Map map = new HashMap();
         try {
 
-            Double radius = 0.05;
+            Double radius = 0.01*distance;
             List<Shop> shopList = shopService.getShopsByRadius(longitude, latitude, radius);
             List<Hairstylist> tempHairstylists = new ArrayList<>();
 
@@ -1312,16 +1313,17 @@ public class HairstylistController {
     }
 
 
-    @ApiOperation(value = "根据经纬度获取周围的发型师", notes = "从附近门店（经纬度相差0.05即约附近5公里左右）中获取发型师列表(按照flag排序：flag=0表示按照发型师的评分降序排序，flag=1表示按发型师已完成订单总数降序排序)")
+    @ApiOperation(value = "根据经纬度获取周围的发型师列表", notes = "从附近门店中获取发型师列表(按照flag排序：flag=0表示按照发型师的评分降序排序，flag=1表示按发型师已完成订单总数降序排序)")
     @GetMapping("/user/getLocalHairstylists")
     public Map getLocalHairstylists(@RequestParam Double longitude,
                                     @RequestParam Double latitude, @RequestParam Integer flag,
+                                    @RequestParam(value = "distance",defaultValue = "5") Integer distance,
                                     @RequestParam(name = "pageNum", defaultValue = "0") int pageNum,
                                     @RequestParam(name = "pageSize", defaultValue = "10") int pageSize) {
         Map map = new HashMap();
         try {
 
-            Double radius = 0.05;
+            Double radius = 0.01*distance;//范围表示为distance公里
             List<Shop> shopList = shopService.getShopsByRadius(longitude, latitude, radius);
             List<HairstylistInfo> resultList = new ArrayList<>();
 
