6d863ebdec5ffdea6a8a5af9d73d8dd1081c7438
1055386758 <1055386758@qq.com>
2020-02-03 20:02:35 +0800

    修改电话号码的判断，原来的电话号段的判断过时了，也没改成最新的，只是改成判断以1开头的11数字

--- src/main/java/com/gaocimi/flashpig/utils/xp/MyUtils.java
+++ src/main/java/com/gaocimi/flashpig/utils/xp/MyUtils.java
@@ -225,7 +225,8 @@ public class MyUtils {
         try {
             //13********* ,15********,18*********
             Pattern p = Pattern
-                    .compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+                    .compile("^(1)\\d{10}$");
+//                    .compile("^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$");
             Matcher m = p.matcher(mobiles);
             flag = m.matches();
         } catch (Exception e) {
