b081b2a4c9d9843b99db2a923b547a16dbdf02e3
1055386758@qq.com <1055386758@qq.com>
2020-05-02 11:20:35 +0800

    数据库中添加产品订单表，后端建立相应实体类

---
+++ src/main/java/com/gaocimi/flashpig/entity/ProductOrder.java
@@ -0,0 +1,99 @@
+package com.gaocimi.flashpig.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * ProductOrder - 产品订单类
+ *
+ * @author xp
+ * @date 2020-5-2 10:54:07
+ */
+@Entity
+@JsonIgnoreProperties(value = {"user"})
+@Table(name = "wx_pay_order")
+@Data
+public class ProductOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 根据一系列参数设计的订单号
+     */
+    @Column(nullable = false)
+    private String orderNumber;
+
+    /**
+     * 提交该订单的用户； 定义名为user_id的外键列，该外键引用user表的主键(id)列,采用懒加载
+     */
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
+
+    /**
+     * 该订单的产品； 定义名为product_id的外键列，该外键引用product表的主键(id)列,采用懒加载
+     */
+    @ManyToOne(targetEntity = Product.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    public Product product;
+
+    /**
+     * 订购的产品数量
+     */
+    @Column(nullable = false)
+    private Integer productQuantity;
+
+    /**
+     * 订单总价格
+     */
+    @Column(nullable = false)
+    private Double totalPrice;
+
+    /**
+     * 用户提交的联系电话（考虑到用户自身没有绑定手机号码）
+     */
+    @Column(nullable = false)
+    private String userPhone;
+
+    /**
+     * 订单总价格
+     */
+    @OneToOne(targetEntity = WxPayOrder.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "pay_order_id")
+    private WxPayOrder wxPayOrder;
+
+    /**
+     * 寄发快递的物流号
+     */
+    private String logisticsNumber;
+
+    /**
+     * 用户的备注
+     */
+    private String remark;
+
+    /**
+     * 支付订单的创建时间
+     */
+    @Column(nullable = false)
+    private Date createTime;
+
+    /**
+     * 订单状态，“0”表示待付款，“1”表示已付款，“-1”表示订单已取消
+     */
+    @Column(nullable = false)
+    private Integer status;
+
+    /**
+     * 订单完成后用户的评价
+     */
+    @Column(nullable = false)
+    private String comment;
+
+}
